f93624aa5554d234d235d372bcf6b26825cbf0e6
Karl J. Overå <k@rl.run>
2026-01-13 10:31:25 +0100

    feat: more node utils and lookups

--- libs/node/src/icd10.ts
+++ libs/node/src/icd10.ts
@@ -6,10 +6,12 @@ export type ICD10Diagnosis = BaseEntry & {
     system: 'ICD10'
 }
 
+export const ICD10_OID = '2.16.578.1.12.4.1.1.7110'
+
 export const ICD10 = icd10.map((it) => ({ ...it, system: 'ICD10' })) satisfies ICD10Diagnosis[]
 
 export function getICD10(code: string): ICD10Diagnosis | null {
-    return ICD10.find((it) => it.code === code) ?? null
+    return ICD10.find((it) => it.code.replace('.', '') === code.replace('.', '')) ?? null
 }
 
 export default ICD10
--- libs/node/src/index.ts
+++ libs/node/src/index.ts
@@ -1,3 +1,33 @@
-export { ICD10, type ICD10Diagnosis } from './icd10'
-export { ICPC2, type ISPC2Diagnosis } from './icpc2'
-export { ICPC2B, type ICPC2BDiagnosis } from './icpc2b'
+import { getICD10, ICD10_OID } from './icd10'
+import { getICPC2, ICPC2_OID } from './icpc2'
+import { getICPC2B, ICPC2B_OID } from './icpc2b'
+
+export { ICD10, ICD10_OID, type ICD10Diagnosis, getICD10 } from './icd10'
+export { ICPC2, ICPC2_OID, type ISPC2Diagnosis, getICPC2 } from './icpc2'
+export { ICPC2B, ICPC2B_OID, type ICPC2BDiagnosis, getICPC2B } from './icpc2b'
+
+export function diagnoseFrom(system: 'ICD10' | 'ICPC2' | 'ICPC2B', code: string) {
+    switch (system) {
+        case 'ICPC2B':
+            return getICPC2B(code)
+        case 'ICPC2':
+            return getICPC2(code)
+        case 'ICD10':
+            return getICD10(code)
+        default:
+            return null
+    }
+}
+
+export function diagnoseFromOid(oid: string, code: string) {
+    switch (oid) {
+        case ICPC2B_OID:
+            return getICPC2B(code)
+        case ICPC2_OID:
+            return getICPC2(code)
+        case ICD10_OID:
+            return getICD10(code)
+        default:
+            return null
+    }
+}
